9763082df0366107c480f76edcdf6fb01e31b724
Lucia994 <l.summa94@gmail.com>
2025-07-01 12:45:10 +0200

    Create a folder with a .js file that contains the array of posts.

---
+++ data/postslist.js
@@ -0,0 +1,37 @@
+const posts = [
+        {
+            title: "Ciambellone",
+            content: "",
+            img: `http://localhost:${port}/images/ciambellone.jpeg`,
+            tags: ["", ""]
+
+        },
+        {
+            title: "Cracker barbabietola",
+            content: "",
+            img: `http://localhost:${port}/images/cracker_barbabietola.jpeg`,
+            tags: ["", ""]
+
+        },
+        {
+            title: "Pane fritto dolce",
+            content: "",
+            img: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
+            tags: ["", ""]
+
+        },
+        {
+            title: "Pasta barbabietola",
+            content: "",
+            img: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
+            tags: ["", ""]
+
+        },
+        {
+            title: "Torta paesana",
+            content: "",
+            img: `http://localhost:${port}/images/torta_paesana.jpeg`,
+            tags: ["", ""]
+
+        }
+    ];
